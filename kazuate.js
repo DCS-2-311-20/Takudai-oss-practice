4e31612f7007e24109dab8ba18510e01a6a11147
G284442022 <228444@st.takushoku-u.ac.jp>
2024-05-30 14:42:32 +0900

    as

--- kazuate.js
+++ kazuate.js
@@ -8,15 +8,16 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', greeting);
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
+
+function greeting() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+	let i = document.querySelector('input[name="yoso"]');
+	let yoso = i.value;
   
   // 課題3-1: 正解判定する
     let p1 = document.querySelector('span#kaisu');
